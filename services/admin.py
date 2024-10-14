fe5fc618e53905ecd44991c9667b28025f168e00
gonyango <grantononyango@gmail.com>
2024-10-14 13:30:13 +0300

    remove: services/admin.py

--- services/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Service
-
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price_hour", "field", "date")
